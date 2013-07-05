a25586c90cdfffb20d58a6e95839b1f377c2da40
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2013-07-05 10:19:35 +0200

    added config files

---
+++ dotvim/dotvim/skeletons/skeleton.h
@@ -0,0 +1,17 @@
+#ifndef <HEADER> 
+#define <HEADER>
+
+namespace Mud {
+
+class <CLASSNAME> {
+public:
+    <CLASSNAME>();
+    virtual ~<CLASSNAME>();
+private:
+    <CLASSNAME>(const <CLASSNAME>&);
+    <CLASSNAME>& operator= (const <CLASSNAME>&);
+};
+
+}
+
+#endif
---
+++ dotvim/skeletons/skeleton.h
@@ -0,0 +1,17 @@
+#ifndef <HEADER> 
+#define <HEADER>
+
+namespace mynamespace {
+
+class <CLASSNAME> {
+public:
+    <CLASSNAME>();
+    virtual ~<CLASSNAME>();
+private:
+    <CLASSNAME>(const <CLASSNAME>&);
+    <CLASSNAME>& operator= (const <CLASSNAME>&);
+};
+
+}
+
+#endif
