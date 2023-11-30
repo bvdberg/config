7022abdc95bfa526f56c093f25b7ed752b052fec
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2023-11-30 06:56:30 +0100

    config

---
+++ dotvim/skeletons/skeleton_vhdl.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity AbcTb is
+end entity;
+
+architecture sim of AcbTb is
+
+begin
+
+    process is
+    begin
+
+        wait for 10 ns;
+
+    end process;
+
+end architecture;
+
